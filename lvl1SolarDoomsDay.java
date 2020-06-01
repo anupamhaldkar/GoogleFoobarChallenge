693d7d1b6b654f80f38cf7bd2e0dbed352aebeb7
Anupam Haldkar <ahdev2020@outlook.com>
2020-06-01 23:30:38 +0530

    Added Solar Doomsday

---
+++ lvl1SolarDoomsDay.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Solution {
+    public static int[] solarPanels(int src) {
+        int[] r = new int[0];
+        while (src > 0) {
+            int closestRoot = getClosestSquareRoot(src);
+
+            if ((Math.pow(closestRoot, 2)) > src)
+                closestRoot -= 1;
+
+            int square = (int) Math.pow(closestRoot, 2);
+            r = Arrays.copyOf(r, r.length+1);
+            r[r.length -1] = square;
+            src = src - square;
+        }
+        return r;
+    }
+
+    public static int getClosestSquareRoot(int src) {
+        int r = (int) (Math.sqrt((double) src) + 0.5);
+        return r;
+    }
+}
