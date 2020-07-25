6050b6ac822aff61b3a0f0798020dbbd552908ec
Anupam Haldkar <ahdev2020@outlook.com>
2020-07-25 23:59:09 +0530

    Update Hey_I_Already_Did_That.py

--- Hey_I_Already_Did_That.py
+++ Hey_I_Already_Did_That.py
@@ -10,3 +10,9 @@ def solution(n,b):
         if len(zlist)<k: n = n.zfill(k)
         if n in zlist: return len(zlist)-zlist.index(n)
         zlist.append(n)
+def toBaseN(num,b):
+    baseNnum=[]
+    while num:
+        baseNnum.insert(0,str(num%b))
+        num=num//b
+    return ''.join(baseNnum)
